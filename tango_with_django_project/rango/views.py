ee92934922d22e3db7d9aa17de2f01f5a9f453b6
Bafna <sidharth@Sidharth-Bafnas-MacBook.local>
2014-09-16 17:41:23 +0530

    Two pages have been created. This was the first time

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says: Hello World! <a href = '/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Here is the about page. <a href = '/rango/'>Index</a>")
+
