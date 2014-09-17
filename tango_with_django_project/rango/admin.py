5c6cd774f4dd67ac8fd0aa9d006cf28f7e43b796
Bafna <sidharth@Sidharth-Bafnas-MacBook.local>
2014-09-17 23:22:50 +0530

    Till this point a database has been created

---
+++ tango_with_django_project/rango/admin.py
@@ -0,0 +1,13 @@
+# This is the admin file. Here you can register your database models with the admin interface
+
+from django.contrib import admin
+from rango.models import Category, Page
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
